a67983dff5ac1e08d89278130ce027c4a8d869d9
yassin <yasenhaji@gmail.com>
2020-04-11 23:55:56 +0200

    Fix next port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const next = require('next')
 const mongoose = require('mongoose');
 const db_connection_url = process.env.DB_CONNECTION_URL;
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
